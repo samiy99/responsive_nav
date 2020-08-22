2fe9e3b6e02c81806f8e010888766432ec11d506
samuel <yemanesam39@gmail.com>
2020-08-23 00:11:47 +0300

    finished the tutorial and learned alot

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_links');
+    const navlinks = document.querySelectorAll('.nav_links li');
+
+    
+    burger.addEventListener('click', ()=>{
+        //toggle nav
+        nav.classList.toggle('nav_active');
+
+        //animating links
+
+        navlinks.forEach((link, index)=>{
+            if (link.style.animation) {
+                link.style.animation=''
+                
+            }else{
+                link.style.animation = `navLinksfade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+            
+
+        });
+        //burger animation
+
+        burger.classList.toggle('animate');
+        
+    });
+
+
+}
+
+navSlide();
+
